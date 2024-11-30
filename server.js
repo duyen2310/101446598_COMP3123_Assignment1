582a4a8866051105285186461bb8f9d43da26163
Truong Thuy Ky Duyen <81848246+duyen2310@users.noreply.github.com>
2024-11-30 15:26:35 -0500

    add cors

--- server.js
+++ server.js
@@ -3,34 +3,42 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const employeeRoute = require('./routes/employee'); 
 const userRoute = require('./routes/user'); 
-require('dotenv').config
+require('dotenv').config();
+const cors = require('cors');
 
-
-// const DB_URL = process.env.DB_URL;
-const DB_URL ="mongodb+srv://truongthuykyduyen:jC5cACo4tsFG3NLf@mycluster.a0u4z.mongodb.net/comp3123_assigment1?retryWrites=true&w=majority&appName=MyCluster";
+// MongoDB connection string
+const DB_URL = "mongodb+srv://truongthuykyduyen:jC5cACo4tsFG3NLf@mycluster.a0u4z.mongodb.net/comp3123_assigment1?retryWrites=true&w=majority&appName=MyCluster";
 
 const app = express();
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(bodyParser.json())
 
+// Use CORS middleware globally
+app.use(cors());
+
+// Middleware to parse incoming request bodies
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// Set mongoose's promise library
 mongoose.Promise = global.Promise;
 
-// TODO - Update your mongoDB Atals Url here to Connect to the database
-mongoose.connect(DB_URL, {
-}).then(() => {
-    console.log("Successfully connected to the database mongoDB Atlas Server");    
+// Connect to MongoDB
+mongoose.connect(DB_URL, {}).then(() => {
+    console.log("Successfully connected to the database MongoDB Atlas Server");    
 }).catch(err => {
     console.log('Could not connect to the database. Exiting now...', err);
     process.exit();
 });
-app.use("/api/v1/user",userRoute);
 
-app.use("/api/v1/emp",employeeRoute);
+// Use routes
+app.use("/api/v1/user", userRoute);
+app.use("/api/v1/emp", employeeRoute);
+
+// Home route
 app.get('/', (req, res) => {
     res.send("<h1>Welcome to Assignment 1</h1>");
 });
 
-
+// Start the server
 app.listen(8081, () => {
     console.log("Server is listening on port 8081");
 });
